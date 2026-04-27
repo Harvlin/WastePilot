9d69064ba47257bb327b65d4826ad57f278bcd91
Harvlin Maximillian <harvlinmaximillian@gmail.com>
2026-04-27 22:03:59 +0700

    update backend init

---
+++ wastepilot/src/main/java/com/project/wastepilot/WastepilotApplication.java
@@ -0,0 +1,13 @@
+package com.project.wastepilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WastepilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WastepilotApplication.class, args);
+	}
+
+}
---
+++ wastepilot/src/test/java/com/project/wastepilot/WastepilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.wastepilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WastepilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
