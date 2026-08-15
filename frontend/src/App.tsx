3cefb0e86040e5e91094125dff3b1614bc652f32
Harvlin Maximillian <harvlinmaximillian@gmail.com>
2026-08-15 23:33:22 +0700

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -76,7 +76,7 @@ const App = () => {
         <Toaster />
         <Sonner />
         <AuthProvider>
-          <BrowserRouter>
+          <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
             <Suspense fallback={<AppRouteFallback />}>
               <Routes>
                 <Route path="/" element={<Index />} />
